46194e173703ab6afb6961d4491fdfa16a0b8076
svenellobo <loborecsven@gmail.com>
2025-01-29 17:27:48 +0100

    Added circleshape and player objects

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+  clock = pygame.time.Clock()
+  dt = 0
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+      
     screen.fill(color="black")
-    pygame.display.flip
+    player.draw(screen)
+    pygame.display.flip()
+    
+    dt = clock.tick(60) / 1000
+    
+    
   print("Starting asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
